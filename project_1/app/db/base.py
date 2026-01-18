89cbfb38a6fa405931ebfa4e0f7ab6f9207045b7
Nagi Ebeid <118565209+lbx554@users.noreply.github.com>
2026-01-18 02:39:40 -0800

    Delete project_1 directory

--- project_1/app/db/base.py
+++
@@ -1,5 +0,0 @@
-# db/base.py
-
-from sqlalchemy.orm import declarative_base
-
-Base = declarative_base()
--- project_1/app/test_db.py
+++
@@ -1,9 +0,0 @@
-from db.session import SessionLocal
-from sqlalchemy import text
-
-db = SessionLocal()
-try:
-    results = db.execute(text("SELECT 1"))
-    print("Database connection successful:", results.scalar())
-finally:
-    db.close()
